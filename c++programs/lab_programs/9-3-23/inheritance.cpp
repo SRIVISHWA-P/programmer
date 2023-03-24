59a64f401b34a6b3532f84c8090ab4a066d500ba
SRIVISHWA P <102619518+SRIVISHWA-P@users.noreply.github.com>
2023-03-24 19:06:44 +0530

    correct prgm

--- c++programs/lab_programs/9-3-23/inheritance.cpp
+++ c++programs/lab_programs/9-3-23/inheritance.cpp
@@ -1,145 +1,110 @@
-#include <iostream>
-#include <string>
+#include<iostream>
 using namespace std;
 
-
-class CommunityMember {
-protected:
-    string name;
-    int age;
-public:
-    CommunityMember(string name, int age) : name(name), age(age) {}
-    void display() {
-        cout << "Name: " << name << endl;
-        cout << "Age: " << age << endl;
-    }
+class CommunityMember{
+	protected:
+		string designation;
+		int ssn;
+		int salary;	
+	public:
+		CommunityMember(int no=0, string desig="",int sal=0) : ssn(no),designation(desig), salary(sal) {
+			cout<<"\nCommunity member added"<<endl;
+		} 
+		
+	    void print(){
+	    	cout<<"\nThe social security number is "<<ssn<<endl;
+	    	cout<<"The designation of community member is "<<designation<<endl;
+	    	cout<<"The salary of the community member is "<<salary<<endl;
+		}
 };
 
-class Employee : public CommunityMember {
-protected:
-    int employeeID;
-public:
-    Employee(string name, int age, int employeeID) : CommunityMember(name, age), employeeID(employeeID) {}
-    void display() {
-        CommunityMember::display();
-        cout << "Employee ID: " << employeeID << endl;
-    }
+class Employee : public CommunityMember{
+	public:
+		Employee(int emp_no,string designation,int given_salary)
+		 : CommunityMember(emp_no,designation,given_salary)
+		{
+			cout<<"\nThe employee object created"<<endl;
+		}
+		
 };
 
-class Faculty : public Employee {
-protected:
-    string department;
-public:
-    Faculty(string name, int age, int employeeID, string department) : Employee(name, age, employeeID), department(department) {}
-    void display() {
-        Employee::display();
-        cout << "Department: " << department << endl;
-    }
+class Student : public CommunityMember{
+	public:
+		Student(int stu_no)
+		 : CommunityMember(stu_no,"Student")
+		{
+			cout<<"\nThe student object created"<<endl;
+		}
 };
 
-class Staff : public Employee {
-protected:
-    string office;
-public:
-    Staff(string name, int age, int employeeID, string office) : Employee(name, age, employeeID), office(office) {}
-    void display() {
-        Employee::display();
-        cout << "Office: " << office << endl;
-    }
+class Alumnus : public CommunityMember{
+	public:
+		Alumnus(int Alum_no)
+		 : CommunityMember(Alum_no,"Alumnus")
+		{
+			cout<<"\nThe Alumnus object created"<<endl;
+		}
 };
 
-class Administrator : public Faculty {
-protected:
-    string title;
-public:
-    Administrator(string name, int age, int employeeID, string department, string title) : Faculty(name, age, employeeID, department), title(title) {}
-    void display() {
-        Faculty::display();
-        cout << "Title: " << title << endl;
-    }
+class Faculty : public Employee{
+	public:
+		Faculty(int Fac_no,string designation,int salary)
+		 : Employee(Fac_no,designation,salary)
+		{
+			cout<<"\nThe Faculty object created"<<endl;
+		}
 };
 
-class Teacher : public Faculty {
-protected:
-    string course;
-public:
-    Teacher(string name, int age, int employeeID, string department, string course) : Faculty(name, age, employeeID, department), course(course) {}
-    void display() {
-        Faculty::display();
-        cout << "Course: " << course << endl;
-    }
+class Staff : public Employee{
+	public:
+		Staff(int st_no,int salary)
+		 : Employee(st_no,"Staff",salary)
+		{
+			cout<<"\nThe Staff object created"<<endl;
+		}
 };
 
-class AdministratorTeacher : public Administrator, public Teacher {
-public:
-    AdministratorTeacher(string name, int age, int employeeID, string department, string title, string course) : Administrator(name, age, employeeID, department, title), Teacher(name, age, employeeID, department, course) {}
-    void display() {
-        Administrator::display();
-        cout << "Course: " << course << endl;
-    }
+class Administrator : virtual public Faculty{
+	public:
+		Administrator(int Admin_no,int salary)
+		 : Faculty(Admin_no,"AdminstratorTeacher",salary)
+		{
+			cout<<"\nThe Administrator object created"<<endl;
+		}	
 };
 
-class Student:public CommunityMember{
-protected:
-int marks;
-char grade;
-public:
-Student(string name,int age,int marks,char grade):CommunityMember(name, age),marks(marks),grade(grade){}
-void display(){
-	CommunityMember::display();
-	cout<<"Marks:"<<marks<<endl;
-	cout<<"Grade:"<<grade<<endl;
-}
+class Teacher:virtual public Faculty{
+	private:
+		int teacher_number;
+		int salary;
+	public:
+		Teacher(int t_no,int salary) : Faculty(t_no,"AdministratorTeacher",salary)
+		{
+			cout<<"\nThe Teacher object created"<<endl;
+		}
 };
 
-class Alumnus:public CommunityMember{
-protected:
-int graduationyear;
-public:
-Alumnus(string name,int age,int year):CommunityMember(name, age),graduationyear(year){}
-void display(){
-	CommunityMember::display();
-	cout<<"Graduated year:"<<graduationyear;
-}
+class AdministratorTeacher : public Administrator,public Teacher
+
+{   public:
+		AdministratorTeacher(int ad_no,int salary)
+		 : Administrator(ad_no,salary), Teacher(ad_no,salary)
+		{
+			cout<<"\nThe AdministratorTeacher object created object created"<<endl;
+		}
 };
 
-int main() {
-    CommunityMember member("John Doe", 30);
-    member.display();
-    cout << endl;
 
-    Employee employee("Jane Smith", 25, 123);
-    employee.display();
-    cout << endl;
-
-    Faculty faculty("Tom Jones", 40, 456, "Mathematics");
-    faculty.display();
-    cout << endl;
-
-    Staff staff("Sarah Lee", 35, 789, "Admissions");
-    staff.display();
-    cout << endl;
-
-    Administrator administrator("Mary Johnson", 50, 111, "Business", "Dean");
-    administrator.display();
-    cout << endl;
-    
-    Teacher teacher("anand",30,344,"IT","OOPS");
-    teacher.display();
-    cout<<endl;
-    
-    AdministratorTeacher administratorteacher("ram",33,355,"IT","HOD","DAA");
-    administratorteacher.display();
-    cout<<endl;
-    
-    Student student("bala",21,99,'A');
-    student.display();
-    cout<<endl;
-    
-    Alumnus alumnus("Seetha",40,2003);
-    alumnus.display();
-    cout<<endl;
-    
-    
-    
+int main(){
+	Student s1(2021115109);
+	s1.print();
+	
+	Alumnus a1(8888);
+	a1.print();
+	
+	Staff revathi(6765,20000);
+	revathi.print();
+	
+	AdministratorTeacher anto(7654,500000);
+	anto.print();
 }
