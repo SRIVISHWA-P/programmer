b3698090acde827eb20ffa82fd57db9c43f31b5d
SRIVISHWA P <srivichu308@gmail.com>
2023-06-26 19:56:15 +0530

    88.py added

--- Leetcode_problems/1.py
+++ Leetcode_problems/1.py
@@ -1,27 +1,26 @@
-#Two sum
-'''Given an array of integers nums and an integer target, return indices 
-of the two numbers such that they add up to target.'''
+# Two sum
+"""Given an array of integers nums and an integer target, return indices 
+of the two numbers such that they add up to target."""
 
 
-nums=[]
+nums = []
 
 
-#populating the array
-n=int(input("How many elements will you enter :"))
+# populating the array
+n = int(input("How many elements will you enter :"))
 for i in range(n):
-    e=eval(input("Enter the element %d :"%(i+1)))
+    e = eval(input("Enter the element %d :" % (i + 1)))
     nums.append(e)
 print("The elements of the array :")
 for j in nums:
-    print(j,end=' ')
+    print(j, end=" ")
 
 
-
-target=int(input("\nEnter tour target:"))
+target = int(input("\nEnter tour target:"))
 d = {}
 for i, j in enumerate(nums):
     r = target - j
-    if r in d: 
+    if r in d:
         print([d[r], i])
         break
     d[j] = i
