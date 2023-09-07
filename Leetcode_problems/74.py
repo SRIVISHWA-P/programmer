5b4a26c0a4f0e2fdc0fd250d588d00b1a14cabc3
SRIVISHWA P <srivichu308@gmail.com>
2023-09-08 00:41:14 +0530

    Create 74.py

---
+++ Leetcode_problems/74.py
@@ -0,0 +1,29 @@
+# set matrix zeros, optimised solution
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        m = len(matrix)
+        n = len(matrix[0])
+        col_0_track = 1
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    if j == 0:
+                        col_0_track = 0
+                    else:
+                        matrix[0][j] = 0
+
+        # This loop is except for first row and first column
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+        if matrix[0][0] == 0:
+            for j in range(n):
+                matrix[0][j] = 0
+        if col_0_track == 0:
+            for i in range(m):
+                matrix[i][0] = 0
