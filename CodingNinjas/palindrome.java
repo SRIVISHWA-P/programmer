26d1d58722a571b32c7b091351bd6cdd9ecee168
SRIVISHWA P <srivichu308@gmail.com>
2024-01-19 19:51:59 +0530

    palindrome program

---
+++ CodingNinjas/palindrome.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Solution {
+    public static boolean palindrome(int num) {
+        int rev = 0;
+        int temp = num;
+        while (temp != 0) {
+            int digit = temp % 10;
+            rev = rev * 10 + digit;
+            temp /= 10;
+        }
+        return (rev == num) ? true : false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(palindrome(num));
+        sc.close();
+    }
+}
