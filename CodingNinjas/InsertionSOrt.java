c09ac1859b599df9b2e2c0875c9d11075d08d03a
SRIVISHWA P <srivichu308@gmail.com>
2024-01-26 13:53:44 +0530

    Insertion sort program

---
+++ CodingNinjas/InsertionSOrt.java
@@ -0,0 +1,13 @@
+class Solution {
+    public static void insertionSort(int[] arr, int size) {
+        for (int i = 1; i < size; i++) {
+            int j = i;
+            while (j > 0 && arr[j] < arr[j - 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+                j--;
+            }
+        }
+    }
+}
