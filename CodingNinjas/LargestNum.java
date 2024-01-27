878790757ae9eda80dd86b2a2a422bd399ae735d
SRIVISHWA P <srivichu308@gmail.com>
2024-01-27 20:29:32 +0530

    Largest number program

---
+++ CodingNinjas/LargestNum.java
@@ -0,0 +1,11 @@
+class Solution {
+
+    static int largestElement(int[] arr, int n) {
+        int maxElement = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > maxElement)
+                maxElement = arr[i];
+        }
+        return maxElement;
+    }
+}
