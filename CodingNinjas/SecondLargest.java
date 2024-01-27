6173ebfa5112924f9d19be9de49bbc4b47b23809
SRIVISHWA P <srivichu308@gmail.com>
2024-01-28 00:15:00 +0530

    updated optimal method

--- CodingNinjas/SecondLargest.java
+++ CodingNinjas/SecondLargest.java
@@ -1,16 +1,18 @@
+//optimal approach using two pointer method
 class Solution {
     public static int[] getSecondOrderElements(int n, int[] a) {
         int largest = a[0], smallest = a[0], sL = -1, sS = Integer.MAX_VALUE;
         for (int i = 1; i < a.length; i++) {
-            if (a[i] > largest)
+            if (a[i] > largest) {
+                sL = largest;
                 largest = a[i];
-            if (a[i] < smallest)
-                smallest = a[i];
-        }
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] > sL && a[i] != largest)
+            } else if (a[i] < largest && a[i] > sL)
                 sL = a[i];
-            if (a[i] < sS && a[i] != smallest)
+
+            if (a[i] < smallest) {
+                sS = smallest;
+                smallest = a[i];
+            } else if (a[i] != smallest && a[i] < sS)
                 sS = a[i];
         }
         int arr[] = { sL, sS };
